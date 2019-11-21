a4dfe94efe4e8bcbcef650b82ca9dbdc01fbe437
Detril <andremoukarzel@gmail.com>
2019-11-21 10:36:54 -0300

    Improve unit ruleset

--- database/units/green_slime.lua
+++ database/units/green_slime.lua
@@ -2,6 +2,10 @@
 return {
   name = "Green Slime",
   max_hp = 4,
-  appearance = 'slime'
+  appearance = 'slime',
+
+  power = 2,
+  range = 1,
+  speed = 20
 }
 
--- ur-proto/rules/unit.lua
+++ ur-proto/rules/unit.lua
@@ -1,43 +1,55 @@
 return function (ruleset)
 
+  local Vec = require 'common.vec'
   local r = ruleset.record
 
-  r:new_property('unit', { power = 1, range = 1, speed = 0 })
+  r:new_property('unit', { spec = nil, hp, power = 1, range = 1, speed = 0, position = Vec() })
 
-  function ruleset.define:new_creature(name, power, range, speed)
+  function ruleset.define:new_unit(specname, initial_position)
     function self.when()
       return true
     end
     function self.apply()
       local e = ruleset:new_entity()
+      local spec = require('database.units.' .. specname)
       r:set(e, 'named', { name = name })
-      r:set(e, 'creature', { power = power, range = range, speed = speed or 0 })
+      r:set(e, 'unit', { spec = spec, hp = spec.max_hp, power = spec.power, range = spec.range or 0,
+                         speed = spec.speed or 0, position = initial_position})
       return e
     end
   end
 
   function ruleset.define:get_power(e)
     function self.when()
-      return r:is(e, 'creature')
+      return r:is(e, 'unit')
     end
     function self.apply()
-      return r:get(e, 'creature', 'power')
+      return r:get(e, 'unit', 'power')
     end
   end
 
   function ruleset.define:get_range(e)
     function self.when()
-      return r:is(e, 'creature')
+      return r:is(e, 'unit')
     end
     function self.apply()
-      return r:get(e, 'creature', 'range')
+      return r:get(e, 'unit', 'range')
+    end
+  end
+
+  function ruleset.define:get_hp(e)
+    function self.when()
+      return r:is(e, 'unit')
+    end
+    function self.apply()
+      return r:get(e, 'unit', 'hp'), r:get(e, 'unit', 'spec').max_hp
     end
   end
 
   --[[
   function ruleset.define:is_dead(e)
     function self.when()
-      return r:is(e, 'creature')
+      return r:is(e, 'unit')
     end
     function self.apply()
       return e.toughness <= 0
@@ -47,10 +59,20 @@ return function (ruleset)
 
   function ruleset.define:get_description(e)
     function self.when()
-      return r:is(e, 'creature')
+      return r:is(e, 'unit')
     end
     function self.apply()
-      return ("%s (%d/%d creature)"):format(e.name, e.power, e.range)
+      return ("%s (%d/%d unit)"):format(e.name, e.power, e.range)
+    end
+  end
+
+  function ruleset.define:calculate_movement(e, all_units, dt)
+    function self.when()
+      return r:is(e, 'unit') and r:get(e, 'unit', 'speed') > 0
+    end
+    function self.apply()
+      local spd = r:get(e, 'unit', 'speed')
+      r:set(e, 'unit', { position = r:get(e, 'unit', 'position'):add(Vec(-spd, spd) * dt) })
     end
   end
 
